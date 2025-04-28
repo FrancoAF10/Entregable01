7f159c35ec9804caf580d21b81ea5fad7c5eba8e
Gian Franco Anton Felix <antonfelixfranco@gmail.com>
2025-04-28 16:21:06 -0500

    AVANCE BASE DE DATOS

---
+++ config/database.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE TIENDA;
+USE TIENDA;
+
+CREATE TABLE CATEGORIAS(
+idcategoria			INT PRIMARY KEY AUTO_INCREMENT,
+categoria		VARCHAR(50) NOT NULL
+)ENGINE=INNODB;
+
+CREATE TABLE SUBCATEGORIAS(
+idsubcategoria					INT PRIMARY KEY AUTO_INCREMENT,
+subcategoria		VARCHAR(50) NOT NULL,
+idcategoria			INT,
+CONSTRAINT fk_categoria FOREIGN KEY (idcategoria) REFERENCES CATEGORIAS(idcategoria)
+)ENGINE=INNODB;
+
+CREATE TABLE MARCAS(
+idmarca				INT PRIMARY KEY AUTO_INCREMENT,
+marca				VARCHAR(50) NOT NULL,
+idsubcategoria		INT,
+CONSTRAINT fk_subcategoria FOREIGN KEY (idsubcategoria) REFERENCES SUBCATEGORIAS(idsubcategoria)
+)ENGINE=INNODB;
+
+CREATE TABLE PRODUCTOS(
+idproducto			INT PRIMARY KEY AUTO_INCREMENT,
+precio				VARCHAR(50) NOT NULL,
+modelo				VARCHAR(50) NOT NULL,
+fechaRegistro		DATE NOT NULL,
+fotografia			VARCHAR(200) NOT NULL,
+idmarca				INT,
+CONSTRAINT fk_marca FOREIGN KEY (idmarca) REFERENCES MARCAS(idmarca)
+)ENGINE=INNODB;
+
+INSERT INTO CATEGORIAS(categoria)VALUES("vestimenta");
+INSERT INTO SUBCATEGORIAS(subcategoria,idcategoria)VALUES("pantalones",1);
+INSERT INTO MARCAS(marca,idsubcategoria)VALUES("LEVI'S",1);
+INSERT INTO PRODUCTOS(precio,modelo,fechaRegistro,fotografia,idmarca)VALUES(125.50,"jeans","2025-04-28","URL",1);
